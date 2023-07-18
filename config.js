4e8c9118aa93bed7f608c7072cf792ebe7690208
irajnethmina12 <139835203+irajnethmina12@users.noreply.github.com>
2023-07-18 17:58:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ import axios from 'axios'
 
 global.owner = [
   ['94773433694', 'GURU', true],
-  ['94720643214', 'INT BROO in', true], 
+  ['94773433694', 'INT BROO in', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['94773433694','94773433694'] 
+global.prems = ['94773433694', '94773433694', '94773433694']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
